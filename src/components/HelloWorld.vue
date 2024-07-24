6f6b1d73e23d55042564fadafe864cd5493c73f8
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-07-24 20:11:55 +0200

    feat: add theme toggle component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,25 +1,25 @@
 <template>
   <div class="hello">
     <h1>{{ message }}</h1>
-    <button @click="toggleTheme">{{ theme === 'dark' ? 'Light Mode' : 'Dark Mode' }}</button>
+    <ThemeToggle />
   </div>
+
+  <p>{{ t('test') }}</p>
+  <select v-model="$i18n.locale">
+    <option v-for="locale in $i18n.availableLocales" :key="locale" :value="locale">
+      {{ locale }}
+    </option>
+  </select>
 </template>
 
 <script lang="ts" setup>
-  import { onMounted, ref } from 'vue';
+  import { ref } from 'vue';
+  import { useI18n } from 'vue-i18n';
+  import ThemeToggle from './theme-toggle.component.vue';
+
+  const { t } = useI18n();
 
   const message = ref('test');
-
-  const theme = ref(localStorage.getItem('theme') || 'light');
-  const toggleTheme = () => {
-    theme.value = theme.value === 'dark' ? 'light' : 'dark';
-    document.documentElement.setAttribute('data-theme', theme.value);
-    localStorage.setItem('theme', theme.value);
-  };
-
-  onMounted(() => {
-    document.documentElement.setAttribute('data-theme', theme.value);
-  });
 </script>
 
 <style scoped lang="scss">
