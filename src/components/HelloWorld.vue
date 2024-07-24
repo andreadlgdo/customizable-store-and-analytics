98ab386a5b55e6685dabecafaaa0c17f199d1a52
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-07-24 19:57:47 +0200

    feat: add dynamic variables

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,28 +1,29 @@
 <template>
   <div class="hello">
     <h1>{{ message }}</h1>
+    <button @click="toggleTheme">{{ theme === 'dark' ? 'Light Mode' : 'Dark Mode' }}</button>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { ref } from 'vue';
+  import { onMounted, ref } from 'vue';
 
   const message = ref('test');
+
+  const theme = ref(localStorage.getItem('theme') || 'light');
+  const toggleTheme = () => {
+    theme.value = theme.value === 'dark' ? 'light' : 'dark';
+    document.documentElement.setAttribute('data-theme', theme.value);
+    localStorage.setItem('theme', theme.value);
+  };
+
+  onMounted(() => {
+    document.documentElement.setAttribute('data-theme', theme.value);
+  });
 </script>
 
 <style scoped lang="scss">
-  h3 {
-    margin: 40px 0 0;
-  }
-  ul {
-    list-style-type: none;
-    padding: 0;
-  }
-  li {
-    display: inline-block;
-    margin: 0 10px;
-  }
-  a {
-    color: #42b983;
+  .hello {
+    background: var(--bg-main);
   }
 </style>
