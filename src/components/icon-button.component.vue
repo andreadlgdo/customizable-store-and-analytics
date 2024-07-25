25043ee74e6524b477345c330ed70e2ab20ccb4d
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-07-25 12:50:25 +0200

    feat: add icon component

---
+++ src/components/icon-button.component.vue
@@ -0,0 +1,71 @@
+<template>
+  <button :class="[baseClass, `${baseClass}--${size}`]" @click="$emit('click')">
+    <img
+      :class="`${baseClass}__icon`"
+      :src="require(`../assets/media/icons/${icon}.svg`)"
+      :alt="icon"
+    />
+  </button>
+</template>
+
+<script lang="ts" setup>
+  import { PropType, toRefs } from 'vue';
+  import { SizeType } from '../types/size.type';
+
+  const baseClass = 'icon-button';
+
+  const props = defineProps({
+    size: {
+      type: String as PropType<SizeType>,
+      default: 'normal'
+    },
+    icon: {
+      type: String,
+      required: true
+    }
+  });
+
+  const { size } = toRefs(props);
+</script>
+
+<style scoped lang="scss">
+  .icon-button {
+    $base-class: &;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    background: transparent;
+    border: none;
+
+    &--small {
+      width: 1.5rem;
+      height: 1.5rem;
+
+      #{$base-class}__icon {
+        width: 1rem;
+        height: 1rem;
+      }
+    }
+
+    &--normal {
+      width: 2rem;
+      height: 2rem;
+
+      #{$base-class}__icon {
+        width: 1.5rem;
+        height: 1.5rem;
+      }
+    }
+
+    &--large {
+      width: 3rem;
+      height: 3rem;
+
+      #{$base-class}__icon {
+        width: 2.5rem;
+        height: 2.5rem;
+      }
+    }
+  }
+</style>
