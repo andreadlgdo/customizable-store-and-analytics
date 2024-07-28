fe471cdda09c1a5e42138b2b42cf2626351d6e87
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-07-28 22:20:54 +0200

    feat: open logic

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,26 @@
 <template>
   <div class="app">
-    <Header />
+    <Header @clickUser="openUserAside" />
     <ThemeToggle />
+    <Aside @close="closeUserAside" :isOpen="isAsideOpen" />
   </div>
 </template>
 
 <script lang="ts" setup>
   import Header from './components/header.component.vue';
+  import Aside from './components/aside.component.vue';
   import ThemeToggle from './components/toggles/theme-toggle.component.vue';
+  import { ref } from 'vue';
+
+  const isAsideOpen = ref(false);
+
+  const openUserAside = () => {
+    isAsideOpen.value = true;
+  };
+
+  const closeUserAside = () => {
+    isAsideOpen.value = false;
+  };
 </script>
 
 <style lang="scss">
