e66d6a487dc4cb3fccab9b2991dae29eaf139833
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-07-31 12:49:27 +0200

    feat: add button component

---
+++ src/components/inputs/button-input.component.vue
@@ -0,0 +1,74 @@
+<template>
+  <button
+    :class="[baseClass, `${baseClass}--${type}`, `${baseClass}--${size}`]"
+    @click="$emit('click')"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script lang="ts" setup>
+  import { PropType } from 'vue';
+  import { ButtonType } from '../../types/button.type';
+  import { SizeType } from '../../types/size.type';
+
+  const baseClass = 'button';
+
+  defineProps({
+    text: {
+      type: String,
+      required: true
+    },
+    type: {
+      type: String as PropType<ButtonType>,
+      default: 'stroke'
+    },
+    size: {
+      type: String as PropType<SizeType>,
+      default: 'normal'
+    }
+  });
+
+  defineEmits(['click']);
+</script>
+
+<style lang="scss" scoped>
+  .button {
+    height: 42px;
+    border-radius: 20px;
+    font-size: 18px;
+    cursor: pointer;
+
+    &--stroke {
+      border: 2px solid var(--color-main);
+      color: var(--color-main);
+      background: var(--bg-main);
+
+      &:hover {
+        border: 3px solid var(--color-main);
+      }
+    }
+
+    &--fill {
+      border: none;
+      color: var(--color-main);
+      background: var(--bg-fill-button);
+
+      &:hover {
+        background: var(--bg-fill-button-hover);
+      }
+    }
+
+    &--small {
+      width: 124px;
+    }
+
+    &--normal {
+      width: 224px;
+    }
+
+    &--large {
+      width: 324px;
+    }
+  }
+</style>
