09ae757d93f7fc067eedfeb4118880f078691bf1
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-07-31 14:50:32 +0200

    feat: add library components

---
+++ src/views/library.view.vue
@@ -0,0 +1,152 @@
+<template>
+  <div :class="baseClass">
+    <h1 :class="`${baseClass}__text ${baseClass}__text--title`">{{ t('library.title') }}</h1>
+    <div>
+      <h3 :class="`${baseClass}__text ${baseClass}__text--subtitle`">
+        {{ t('library.toggle.language') }}
+      </h3>
+      <LanguageToggle />
+    </div>
+    <hr />
+    <div>
+      <h3 :class="`${baseClass}__text ${baseClass}__text--subtitle`">
+        {{ t('library.toggle.language') }}
+      </h3>
+      <ThemeToggle />
+    </div>
+    <hr />
+    <div>
+      <h3 :class="`${baseClass}__text ${baseClass}__text--subtitle`">
+        {{ t('library.asides.title') }}
+      </h3>
+      <button-input :text="isAsideOpen ? 'Close aside' : 'Open aside'" @click="chageUserAside" />
+      <Aside :is-open="isAsideOpen" @close="chageUserAside" />
+    </div>
+    <hr />
+    <div>
+      <h3 :class="`${baseClass}__text ${baseClass}__text--subtitle`">
+        {{ t('library.icons.button') }}
+      </h3>
+      <div :class="`${baseClass}__wrapper ${baseClass}__wrapper--row`">
+        <icon-button icon="search" size="small" colorAttribute="fill" />
+        <icon-button icon="search" colorAttribute="fill" />
+        <icon-button icon="search" size="large" colorAttribute="fill" />
+      </div>
+      <h3 :class="`${baseClass}__text ${baseClass}__text--subtitle`">
+        {{ t('library.icons.svg') }}
+      </h3>
+      <div :class="`${baseClass}__wrapper ${baseClass}__wrapper--row`">
+        <svg-icon
+          :src="require('../assets/media/icons/user.svg')"
+          size="small"
+          colorAttribute="stroke"
+        />
+        <svg-icon :src="require('../assets/media/icons/user.svg')" colorAttribute="stroke" />
+        <svg-icon
+          :src="require('../assets/media/icons/user.svg')"
+          size="large"
+          colorAttribute="stroke"
+        />
+      </div>
+    </div>
+    <hr />
+    <div>
+      <h3 :class="`${baseClass}__text ${baseClass}__text--subtitle`">
+        {{ t('library.inputs.text.title') }}
+      </h3>
+      <div :class="`${baseClass}__wrapper ${baseClass}__wrapper--column`">
+        <text-input />
+        <text-input icon="user" colorAttribute="stroke" />
+        <text-input icon="user" colorAttribute="stroke" placeholder="Placeholder text" />
+        <p>{{ t('library.inputs.text.password') }}</p>
+        <text-input type="password" />
+      </div>
+    </div>
+    <hr />
+    <div>
+      <h3 :class="`${baseClass}__text ${baseClass}__text--subtitle`">
+        {{ t('library.inputs.button') }}
+      </h3>
+      <div :class="`${baseClass}__wrapper ${baseClass}__wrapper--column`">
+        <div :class="`${baseClass}__wrapper ${baseClass}__wrapper--row`">
+          <button-input text="Stroke small" size="small" />
+          <button-input text="Stroke normal" />
+          <button-input text="Stroke large" size="large" />
+        </div>
+        <div :class="`${baseClass}__wrapper ${baseClass}__wrapper--row`">
+          <button-input text="Fill small" type="fill" size="small" />
+          <button-input text="Fill normal" type="fill" />
+          <button-input text="Fill large" type="fill" size="large" />
+        </div>
+      </div>
+    </div>
+    <hr />
+    <div>
+      <h3 :class="`${baseClass}__text ${baseClass}__text--subtitle`">
+        {{ t('library.header.title') }}
+      </h3>
+      <Header />
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { useI18n } from 'vue-i18n';
+  import ThemeToggle from '../components/toggles/theme-toggle.component.vue';
+  import LanguageToggle from '../components/toggles/language-toggle.component.vue';
+  import IconButton from '../components/icons/icon-button.component.vue';
+  import SvgIcon from '../components/icons/svg-icon.component.vue';
+  import TextInput from '../components/inputs/text-input.component.vue';
+  import ButtonInput from '../components/inputs/button-input.component.vue';
+  import Aside from '../components/aside.component.vue';
+  import Header from '../components/header.component.vue';
+
+  import { ref } from 'vue';
+
+  const { t } = useI18n();
+
+  const baseClass = 'library';
+
+  const isAsideOpen = ref(false);
+
+  const chageUserAside = () => {
+    isAsideOpen.value = !isAsideOpen.value;
+  };
+</script>
+
+<style scoped lang="scss">
+  .library {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding: 2rem;
+
+    &__text {
+      font-weight: 600;
+      margin-bottom: 1rem;
+
+      &--title {
+        align-self: center;
+        font-size: xxx-large;
+      }
+
+      &--subtitle {
+        font-size: x-large;
+      }
+    }
+
+    &__wrapper {
+      display: flex;
+
+      &--row {
+        align-items: center;
+        gap: 1.5rem;
+      }
+
+      &--column {
+        flex-direction: column;
+        gap: 1rem;
+      }
+    }
+  }
+</style>
