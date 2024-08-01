029539374f8fd66031cd677851c473993c1630e8
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-08-01 14:01:55 +0200

    feat: use router view

---
+++ src/views/home.view.vue
@@ -0,0 +1,16 @@
+<template>
+  <Header @clickUser="isAsideOpen = true" />
+  <ThemeToggle />
+  <Aside @close="isAsideOpen = false" :isOpen="isAsideOpen" />
+</template>
+
+<script lang="ts" setup>
+  import ThemeToggle from '../components/toggles/theme-toggle.component.vue';
+  import Header from '../components/header.component.vue';
+  import Aside from '../components/aside.component.vue';
+  import { ref } from 'vue';
+
+  const isAsideOpen = ref(false);
+</script>
+
+<style lang="scss" scoped></style>
