903ed483a564f04593cb5ccf4fb31f05c00826cf
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-08-05 13:42:35 +0200

    feat: add asides logic to open and close it

--- src/views/home.view.vue
+++ src/views/home.view.vue
@@ -1,21 +1,47 @@
 <template>
-  <Header @clickUser="isAsideOpen = true" />
+  <Header @clickUser="isLogInAsideOpen = true" />
   <theme-toggle />
   <language-toggle />
-  <LogInAside @close="isAsideOpen = false" :isOpen="isAsideOpen" />
+  <LogInAside
+    @close="closeAllAsides"
+    @openSignUpAsideOpen="openSignUpAsideOpen"
+    :isOpen="isLogInAsideOpen"
+  />
+  <SignUpAside
+    @close="closeAllAsides"
+    @openLogInAsideOpen="openLogInAsideOpen"
+    :isOpen="isSignUpAsideOpen"
+  />
 </template>
 
 <script lang="ts" setup>
   import { ref } from 'vue';
 
   import LogInAside from '../components/asides/log-in-aside.component.vue';
+  import SignUpAside from '../components/asides/sign-up-aside.component.vue';
 
   import ThemeToggle from '../components/toggles/theme-toggle.component.vue';
   import LanguageToggle from '../components/toggles/language-toggle.component.vue';
 
   import Header from '../components/header.component.vue';
 
-  const isAsideOpen = ref(false);
+  const isLogInAsideOpen = ref(false);
+  const isSignUpAsideOpen = ref(false);
+
+  const openSignUpAsideOpen = () => {
+    isLogInAsideOpen.value = false;
+    isSignUpAsideOpen.value = true;
+  };
+
+  const openLogInAsideOpen = () => {
+    isLogInAsideOpen.value = true;
+    isSignUpAsideOpen.value = false;
+  };
+
+  const closeAllAsides = () => {
+    isLogInAsideOpen.value = false;
+    isSignUpAsideOpen.value = false;
+  };
 </script>
 
 <style lang="scss" scoped></style>
