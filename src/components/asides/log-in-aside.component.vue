30e596f03d9e589a9486f681e63f3cc2704da059
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-08-05 11:02:32 +0200

    feat: add log in aside component

---
+++ src/components/asides/log-in-aside.component.vue
@@ -0,0 +1,84 @@
+<template>
+  <Aside @close="$emit('close')" :isOpen="isOpen">
+    <div :class="baseClass">
+      <h1>{{ t('initSesion.title') }}</h1>
+      <div>
+        <text-input
+          :class="`${baseClass}__input ${baseClass}__input--user`"
+          :placeholder="t('initSesion.inputsPlaceholders.user')"
+          icon="user"
+          colorAttribute="stroke"
+        />
+        <text-input
+          :class="`${baseClass}__input ${baseClass}__input--password`"
+          :placeholder="t('initSesion.inputsPlaceholders.password')"
+          type="password"
+          icon="password"
+          colorAttribute="fill"
+        />
+        <p :class="`${baseClass}__text ${baseClass}__text--password`">
+          {{ t('initSesion.forgotPassword') }}
+        </p>
+      </div>
+      <div>
+        <button-input :text="t('initSesion.action')" type="fill" />
+        <p :class="`${baseClass}__text ${baseClass}__text--create-account`">
+          {{ t('initSesion.register.description') }}
+        </p>
+        <a :class="`${baseClass}__text ${baseClass}__text--register`" @click="$emit('register')">
+          {{ t('initSesion.register.action') }}
+        </a>
+      </div>
+    </div>
+  </Aside>
+</template>
+
+<script lang="ts" setup>
+  import Aside from './aside.component.vue';
+  import TextInput from '../inputs/text-input.component.vue';
+  import ButtonInput from '../inputs/button-input.component.vue';
+  import { useI18n } from 'vue-i18n';
+
+  const baseClass = 'log-in-aside';
+
+  const { t } = useI18n();
+
+  defineProps({
+    isOpen: Boolean
+  });
+
+  defineEmits(['close', 'register']);
+</script>
+
+<style lang="scss" scoped>
+  .log-in-aside {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 4rem;
+    margin-top: 42px;
+
+    &__input {
+      &--password {
+        margin-top: 32px;
+      }
+    }
+
+    &__text {
+      display: flex;
+      justify-content: center;
+
+      &--password,
+      &--create-account {
+        margin-top: 16px;
+      }
+
+      &--register {
+        margin-top: 4px;
+        text-decoration: underline;
+        cursor: pointer;
+      }
+    }
+  }
+</style>
--- src/views/home.view.vue
+++ src/views/home.view.vue
@@ -1,15 +1,17 @@
 <template>
   <Header @clickUser="isAsideOpen = true" />
   <theme-toggle />
-  <Aside @close="isAsideOpen = false" :isOpen="isAsideOpen" />
+  <language-toggle />
+  <LogInAside @close="isAsideOpen = false" :isOpen="isAsideOpen" />
 </template>
 
 <script lang="ts" setup>
   import { ref } from 'vue';
 
   import ThemeToggle from '../components/toggles/theme-toggle.component.vue';
+  import LanguageToggle from '../components/toggles/language-toggle.component.vue';
   import Header from '../components/header.component.vue';
-  import Aside from '../components/aside.component.vue';
+  import LogInAside from '../components/asides/log-in-aside.component.vue';
 
   const isAsideOpen = ref(false);
 </script>
