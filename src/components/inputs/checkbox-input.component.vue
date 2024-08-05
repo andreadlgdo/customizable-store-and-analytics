9d33acab85f6be322003a6a33e926b552e1d7576
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-08-05 13:42:49 +0200

    feat: add checkbox input

---
+++ src/components/inputs/checkbox-input.component.vue
@@ -0,0 +1,64 @@
+<template>
+  <div :class="[baseClass, `${baseClass}--${size}`]">
+    <input :class="`${baseClass}__input`" type="checkbox" />
+    <p :class="`${baseClass}__text`">{{ text }}</p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { PropType } from 'vue';
+  import { SizeType } from '../../types/size.type';
+
+  const baseClass = 'checkbox-input';
+
+  defineProps({
+    text: String,
+    size: {
+      type: String as PropType<SizeType>,
+      default: 'normal'
+    }
+  });
+</script>
+
+<style lang="scss" scoped>
+  .checkbox-input {
+    $base-class: &;
+
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+
+    &--small {
+      #{$base-class}__input {
+        width: 12px;
+        height: 12px;
+      }
+
+      #{$base-class}__text {
+        font-size: 12px;
+      }
+    }
+
+    &--normal {
+      #{$base-class}__input {
+        width: 14px;
+        height: 14px;
+      }
+
+      #{$base-class}__text {
+        font-size: 16px;
+      }
+    }
+
+    &--large {
+      #{$base-class}__input {
+        width: 14px;
+        height: 14px;
+      }
+
+      #{$base-class}__text {
+        font-size: 20px;
+      }
+    }
+  }
+</style>
