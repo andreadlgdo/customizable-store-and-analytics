64dd016ccef804efb6f467acc1d4a96c6ac63842
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-08-22 22:24:35 +0200

    feat: add base structure

---
+++ src/components/product-cards/product-card-whist-list.component.vue
@@ -0,0 +1,14 @@
+<template>
+  <div :class="baseClass"></div>
+</template>
+
+<script lang="ts" setup>
+  import { ref } from 'vue';
+
+  const baseClass = ref('product-card-whist-list');
+</script>
+
+<style lang="scss" scoped>
+  .product-card-whist-list {
+  }
+</style>
