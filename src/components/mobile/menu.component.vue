d2e4cf70b07b1ec5992f9b53ad4bd3a48b326523
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-08-27 15:37:16 +0200

    feat: add submenu component

--- src/components/mobile/menu.component.vue
+++ src/components/mobile/menu.component.vue
@@ -1,7 +1,7 @@
 <template>
   <Aside @close="$emit('close')" :is-open="isOpen" close-position="left">
     <div :class="baseClass">
-      <div :class="`${baseClass}__item ${baseClass}__item--user`">
+      <div v-if="!isSubmenuOpen" :class="`${baseClass}__item ${baseClass}__item--user`">
         <svg-icon :src="require('../../assets/media/icons/user.svg')" color-attribute="stroke" />
         <p
           @click="$emit('clickUserAsideOnMobile')"
@@ -10,25 +10,45 @@
           {{ t('mobile.menu.login').toUpperCase() }}
         </p>
       </div>
-      <div
-        v-for="(menu, index) in menuItems"
-        :key="index"
-        :class="`${baseClass}__item ${baseClass}__item--menu`"
-      >
-        <p>{{ menu.label }}</p>
-        <svg-icon
-          v-if="menu.subMenu.length"
-          :src="require('../../assets/media/icons/arrow-right.svg')"
-          color-attribute="fill"
-          size="mini"
-        />
+      <div v-for="(menu, index) in menuItems" :key="index">
+        <div v-if="!isSubmenuOpen" :class="`${baseClass}__item ${baseClass}__item--menu`">
+          <p>{{ menu.label }}</p>
+          <svg-icon
+            v-if="menu.subMenu.length"
+            @click="isSubmenuOpen = true"
+            :src="require('../../assets/media/icons/arrow-right.svg')"
+            color-attribute="fill"
+            size="mini"
+          />
+        </div>
+        <div v-else>
+          <div v-if="menu.subMenu.length" :class="`${baseClass}__item ${baseClass}__item--user`">
+            <svg-icon
+              @click="isSubmenuOpen = false"
+              :class="`${baseClass}__icon`"
+              :src="require('../../assets/media/icons/arrow-right.svg')"
+              color-attribute="fill"
+              size="small"
+            />
+            <p :class="`${baseClass}__text ${baseClass}__text--login`">
+              {{ menu.label.toUpperCase() }}
+            </p>
+          </div>
+          <div
+            v-for="(category, index) in menu.subMenu"
+            :key="index"
+            :class="`${baseClass}__item ${baseClass}__item--submenu`"
+          >
+            <p>{{ category }}</p>
+          </div>
+        </div>
       </div>
     </div>
   </Aside>
 </template>
 
 <script lang="ts" setup>
-  import { PropType } from 'vue';
+  import { PropType, ref } from 'vue';
   import { useI18n } from 'vue-i18n';
 
   import { MenuItem } from '../../interfaces/menuItem';
@@ -39,6 +59,8 @@
   const baseClass = 'menu';
 
   const { t } = useI18n();
+
+  const isSubmenuOpen = ref(false);
 
   defineProps({
     isOpen: Boolean,
@@ -76,6 +98,10 @@
       &--login {
         font-weight: bold;
       }
+    }
+
+    &__icon {
+      transform: rotate(180deg);
     }
   }
 </style>
