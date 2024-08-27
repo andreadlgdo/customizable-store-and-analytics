7909267c1a06f8c96976d3ec95c07f80be75f57b
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-08-27 15:08:32 +0200

    feat: add menu component

--- src/components/mobile/menu.component.vue
+++ src/components/mobile/menu.component.vue
@@ -1,22 +1,81 @@
 <template>
-  <Aside @close="$emit('close')" :is-open="isOpen">
-    <div :class="baseClass"></div>
+  <Aside @close="$emit('close')" :is-open="isOpen" close-position="left">
+    <div :class="baseClass">
+      <div :class="`${baseClass}__item ${baseClass}__item--user`">
+        <svg-icon :src="require('../../assets/media/icons/user.svg')" color-attribute="stroke" />
+        <p
+          @click="$emit('clickUserAsideOnMobile')"
+          :class="`${baseClass}__text ${baseClass}__text--login`"
+        >
+          {{ t('mobile.menu.login').toUpperCase() }}
+        </p>
+      </div>
+      <div
+        v-for="(menu, index) in menuItems"
+        :key="index"
+        :class="`${baseClass}__item ${baseClass}__item--menu`"
+      >
+        <p>{{ menu.label }}</p>
+        <svg-icon
+          v-if="menu.subMenu.length"
+          :src="require('../../assets/media/icons/arrow-right.svg')"
+          color-attribute="fill"
+          size="mini"
+        />
+      </div>
+    </div>
   </Aside>
 </template>
 
 <script lang="ts" setup>
+  import { PropType } from 'vue';
+  import { useI18n } from 'vue-i18n';
+
+  import { MenuItem } from '../../interfaces/menuItem';
+
   import Aside from '../asides/aside.component.vue';
+  import SvgIcon from '../icons/svg-icon.component.vue';
 
   const baseClass = 'menu';
 
+  const { t } = useI18n();
+
   defineProps({
-    isOpen: Boolean
+    isOpen: Boolean,
+    menuItems: {
+      type: Array as PropType<Array<MenuItem>>,
+      default: () => []
+    }
   });
 
-  defineEmits(['close']);
+  defineEmits(['close', 'clickUser']);
 </script>
 
 <style lang="scss" scoped>
   .menu {
+    display: flex;
+    flex-direction: column;
+    margin: 26px 46px;
+
+    &__item {
+      display: flex;
+      align-items: center;
+      border-bottom: 1px solid var(--color-medium);
+      padding: 1rem 1.5rem;
+
+      &--user {
+        gap: 0.5rem;
+      }
+
+      &--menu {
+        justify-content: space-between;
+      }
+    }
+
+    &__text {
+      &--login {
+        font-weight: bold;
+      }
+    }
   }
 </style>
