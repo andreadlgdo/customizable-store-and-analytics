a22ebbf15aa345f98172f79b2a78fb170e5e5cdf
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-09-14 11:07:38 +0200

    feat: add auth methos on login

--- src/components/asides/log-in-aside.component.vue
+++ src/components/asides/log-in-aside.component.vue
@@ -1,15 +1,17 @@
 <template>
   <Aside @close="$emit('close')" :is-open="isOpen" :close-position="closePosition">
-    <div :class="baseClass">
+    <div v-if="!isUserRegister" :class="baseClass">
       <h1>{{ t('userAsides.logIn.title') }}</h1>
       <div>
         <text-input
-          :class="`${baseClass}__input ${baseClass}__input--email`"
-          :placeholder="t('userAsides.logIn.inputsPlaceholders.email')"
-          icon="email"
-          color-attribute="fill"
+          @input="query => (userForm.username = query)"
+          :class="`${baseClass}__input ${baseClass}__input--user`"
+          :placeholder="t('userAsides.logIn.inputsPlaceholders.username')"
+          icon="user"
+          color-attribute="stroke"
         />
         <text-input
+          @input="query => (userForm.password = query)"
           :class="`${baseClass}__input ${baseClass}__input--password`"
           :placeholder="t('userAsides.logIn.inputsPlaceholders.password')"
           type="password"
@@ -21,7 +23,7 @@
         </p>
       </div>
       <div>
-        <button-input :text="t('userAsides.logIn.action')" type="fill" />
+        <button-input @click="logInUser" :text="t('userAsides.logIn.action')" type="fill" />
         <p :class="`${baseClass}__text ${baseClass}__text--create-account`">
           {{ t('userAsides.logIn.signUp.description') }}
         </p>
@@ -33,13 +35,18 @@
         </a>
       </div>
     </div>
+    <!-- TO DO: Change when we add the admin menu -->
+    <div v-else :class="baseClass">
+      <p>ENTRO</p>
+    </div>
   </Aside>
 </template>
 
 <script lang="ts" setup>
-  import { PropType } from 'vue';
+  import { PropType, ref } from 'vue';
   import { useI18n } from 'vue-i18n';
 
+  import { useUsers } from '../../composables/use-users';
   import { PositionType } from '../../types/position.type';
 
   import TextInput from '../inputs/text-input.component.vue';
@@ -51,6 +58,8 @@
 
   const { t } = useI18n();
 
+  const { user, login } = useUsers();
+
   defineProps({
     isOpen: Boolean,
     closePosition: {
@@ -60,6 +69,20 @@
   });
 
   defineEmits(['close', 'openSignUpAsideOpen']);
+
+  const userForm = ref({
+    username: '',
+    password: ''
+  });
+
+  const isUserRegister = ref(false);
+
+  const logInUser = async (): Promise<void> => {
+    if (userForm.value.username && userForm.value.password) {
+      await login(userForm.value.username, userForm.value.password);
+      isUserRegister.value = !!user.value;
+    }
+  };
 </script>
 
 <style lang="scss" scoped>
