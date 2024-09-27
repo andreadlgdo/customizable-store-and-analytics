33e3400ab1b2768f5e0783a312f22946a2b17e53
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-09-27 17:10:44 +0200

    fix: fix imports

--- src/components/asides/menu-user-aside.component.vue
+++ src/components/asides/menu-user-aside.component.vue
@@ -26,13 +26,13 @@
 <script lang="ts" setup>
   import { PropType, ref } from 'vue';
   import { useI18n } from 'vue-i18n';
+  import { useRouter } from 'vue-router';
 
   import { User } from '../../interfaces/user';
 
   import Aside from '../asides/aside.component.vue';
   import MenuItems from '../menu/menu-items.component.vue';
   import ButtonInput from '../inputs/button-input.component.vue';
-  import { useRouter } from 'vue-router';
 
   const baseClass = 'menu-user-aside';
 
