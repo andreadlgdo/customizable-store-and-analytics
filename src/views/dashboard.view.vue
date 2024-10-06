96cf967efa10aaedc3476b4edec45caee0b41d80
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-10-06 10:01:43 +0200

    feat: add base menu

--- src/views/dashboard.view.vue
+++ src/views/dashboard.view.vue
@@ -1,17 +1,61 @@
 <template>
   <header-layout />
   <div :class="baseClass">
-    DASHBOARD
+    <div :class="`${baseClass}__menu`">
+      <div :class="`${baseClass}__user-image`">
+        <img
+          :class="`${baseClass}__image`"
+          :src="user?.imageUrl ?? require('../assets/media/images/empty.png')"
+          alt="userImage"
+        />
+      </div>
+      <menu-items :menu-items="menuElements" showDescription />
+    </div>
+    <div :class="`${baseClass}__landing`"></div>
   </div>
 </template>
 
 <script lang="ts" setup>
+  import { useUsers } from '../composables/use-users';
+  import { useUserMenu } from '../composables/use-user-menu';
+
   import HeaderLayout from './header-layout.view.vue';
+  import MenuItems from '../components/menu/menu-items.component.vue';
 
   const baseClass = 'dashboard';
+
+  const { user } = useUsers();
+
+  const { menuElements } = useUserMenu();
 </script>
 
 <style lang="scss" scoped>
   .dashboard {
+    display: flex;
+
+    &__menu {
+      display: flex;
+      flex-direction: column;
+      justify-content: center;
+      width: 30%;
+    }
+
+    &__landing {
+      width: 70%;
+      background: green;
+    }
+
+    &__user-image {
+      display: flex;
+      justify-content: center;
+      padding: 2rem 0;
+      border-bottom: 1px solid var(--color-medium);
+    }
+
+    &__image {
+      width: 14rem;
+      height: 14rem;
+      border-radius: 50%;
+    }
   }
 </style>
