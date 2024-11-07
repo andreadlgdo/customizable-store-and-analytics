1c71d70ee9bbda6ab6eaa367a1454fe7ec646a90
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-11-07 13:11:39 +0100

    feat: add list items component

--- src/components/index.ts
+++ src/components/index.ts
@@ -9,3 +9,4 @@ export * from './toggles';
 
 export { default as BaseText } from './base-text.component.vue';
 export { default as CategoriesSection } from './categories-section.component.vue';
+export { default as ListItems } from './list-items.component.vue';
