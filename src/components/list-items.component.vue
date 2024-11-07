a575bb4121edda67402a3ab96d762b5b76764937
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-11-07 22:16:23 +0100

    feat: add submenu without photos

--- src/components/list-items.component.vue
+++ src/components/list-items.component.vue
@@ -1,21 +1,39 @@
 <template>
-  <section :class="baseClass">
-    <div v-for="(item, index) in items" :key="index" :class="`${baseClass}__item`">
-      <base-text tag="h4" :class="`${baseClass}__text`">{{ item.label }}</base-text>
-      <icon-button v-if="item.subMenu" icon="arrow" size="small" />
+  <section v-for="item in items" :key="item.id" :class="baseClass">
+    <div :class="`${baseClass}__item`">
+      <base-text tag="h4" :class="`${baseClass}__text ${baseClass}__text--item`">
+        {{ item.label }}
+      </base-text>
+      <icon-button
+        v-if="item.subItem"
+        :icon="expansible ? 'plus' : 'arrow'"
+        size="small"
+        @click="expansible ? (isExpand = !isExpand) : $emit('clickSubItem', item)"
+      />
+    </div>
+    <div v-if="item.subItem && expansible && isExpand" :class="`${baseClass}__subItem`">
+      <base-text
+        v-for="(subItem, index) in item.subItem"
+        :key="index"
+        tag="p"
+        :class="`${baseClass}__text ${baseClass}__text--subItem`"
+      >
+        {{ subItem.label }}
+      </base-text>
     </div>
   </section>
 </template>
 
 <script lang="ts" setup>
-  import { PropType } from 'vue';
+  import { PropType, ref } from 'vue';
 
   import BaseText from './base-text.component.vue';
   import { IconButton } from './icons';
 
   interface Item {
+    id: number;
     label: string;
-    subMenu?: string[];
+    subItem?: Item[];
     image?: string;
   }
 
@@ -25,8 +43,11 @@
     items: {
       type: Array as PropType<Item[]>,
       required: true
-    }
+    },
+    expansible: Boolean
   });
+
+  const isExpand = ref(false);
 </script>
 
 <style lang="scss" scoped>
@@ -34,21 +55,36 @@
     display: flex;
     width: 100%;
     flex-direction: column;
+    padding: 16px;
+    border-bottom: 1px solid var(--color-medium);
 
     &__item {
       display: flex;
       align-items: center;
       justify-content: space-between;
-      padding: 16px;
-      border-bottom: 1px solid var(--color-medium);
     }
 
     &__text {
       cursor: pointer;
 
-      &:hover {
-        color: var(--color-border-primary);
+      &--item {
+        &:hover {
+          color: var(--color-border-primary);
+        }
       }
+
+      &--subItem {
+        &:hover {
+          font-weight: bold;
+        }
+      }
+    }
+
+    &__subItem {
+      display: flex;
+      flex-direction: column;
+      gap: 8px;
+      padding: 8px;
     }
   }
 </style>
