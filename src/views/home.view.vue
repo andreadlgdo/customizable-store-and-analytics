86e7a3a9c8247511f458e595da00fe08c6adaf4c
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-11-14 22:27:44 +0100

    feat: add categories section to the landing

--- src/views/home.view.vue
+++ src/views/home.view.vue
@@ -6,35 +6,49 @@
     :style="{ backgroundImage: `url(${landingImage.imageUrl})` }"
   ></section>
   <categories-carousel :items="categories" />
+  <div :class="`${baseClass}__categories`">
+    <categories-section
+      v-for="category in sections"
+      :key="category?._id"
+      :text="category?.title"
+      :image="category?.imageUrl"
+      size="large"
+      have-shadow
+      :class="`${baseClass}__category`"
+    />
+  </div>
 </template>
 
 <script lang="ts" setup>
-  import { computed, ref, onMounted } from 'vue';
+  import { ref, onMounted } from 'vue';
 
-  import { CategoriesCarousel } from '../components';
+  import { CategoriesCarousel, CategoriesSection } from '../components';
   import { useCategories, useMobile } from '../composables';
   import { generalService } from '../services';
 
   import HeaderLayout from './header-layout.view.vue';
+  import { CategoryEnum } from '@/enums';
 
   const baseClass = 'home';
 
-  const { categories, loadCategories } = useCategories();
+  const { categories, loadCategories, getCategoriesByFilter } = useCategories();
   const { isMobile } = useMobile();
 
-  const images = ref([]);
+  const landingImage = ref();
 
-  const landingImage = computed(() => {
-    if (isMobile.value) {
-      return images.value.find((image: any) => image.device === 'mobile');
-    } else {
-      return images.value.find((image: any) => image.device === 'desktop');
-    }
-  });
+  const sections = ref();
 
   onMounted(async () => {
-    images.value = await generalService.getLandingImages();
+    landingImage.value = await generalService.getLandingImage();
     await loadCategories();
+    const sectionsImages = await generalService.getSectionsImages();
+    const sectionsCategories = getCategoriesByFilter([
+      CategoryEnum.JACKETS,
+      CategoryEnum.ACCESSORIES
+    ]);
+    sections.value = isMobile.value
+      ? [sectionsCategories[0], sectionsImages[0], sectionsCategories[1], sectionsImages[1]]
+      : [sectionsCategories[0], sectionsImages[0], sectionsImages[1], sectionsCategories[1]];
   });
 </script>
 
@@ -46,6 +60,23 @@
       background-size: cover;
       background-position: center;
       background-repeat: no-repeat;
+    }
+
+    &__categories {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+    }
+
+    &__category {
+      margin: 1rem;
+    }
+  }
+
+  @media only screen and (max-width: 500px) {
+    .home {
+      &__categories {
+        grid-template-columns: 1fr;
+      }
     }
   }
 </style>
