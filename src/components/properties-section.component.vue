92dd93dfee45e4c9c82d440d1a9164f134199fff
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-11-18 18:31:58 +0100

    fix: fix properties section on landing

--- src/components/properties-section.component.vue
+++ src/components/properties-section.component.vue
@@ -6,11 +6,16 @@
         :key="index"
         :class="`${baseClass}__property ${baseClass}__property--${index}`"
       >
-        <svg-icon :src="require(`../assets/media/icons/properties/${item.icon}.svg`)" size="none" />
+        <svg-icon
+          :src="require(`../assets/media/icons/properties/${item.icon}.svg`)"
+          size="none"
+          :class="`${baseClass}__svg`"
+        />
         <base-text tag="h4">{{ item.label }}</base-text>
       </section>
     </div>
     <div
+      v-if="!isMobileAndTable"
       :class="`${baseClass}__image`"
       :style="{ backgroundImage: `url(${backgroundImageUrl})` }"
     ></div>
@@ -19,6 +24,8 @@
 
 <script lang="ts" setup>
   import { PropType } from 'vue';
+
+  import { useMobile } from '../composables';
 
   import BaseText from './base-text.component.vue';
   import { SvgIcon } from './icons';
@@ -40,6 +47,8 @@
       required: true
     }
   });
+
+  const { isMobileAndTable } = useMobile();
 </script>
 
 <style lang="scss" scoped>
@@ -62,7 +71,6 @@
       position: absolute;
       display: flex;
       justify-content: space-evenly;
-      width: 100%;
       gap: 6rem;
       padding: 0 2rem;
       overflow-x: hidden;
@@ -96,6 +104,52 @@
       background-position: center;
       background-repeat: no-repeat;
       clip-path: polygon(0 50%, 50% 60%, 100% 50%, 100% 100%, 0% 100%);
+    }
+  }
+
+  @media only screen and (max-width: 1211px) {
+    .properties-section {
+      height: 48rem;
+
+      &__properties {
+        flex-wrap: wrap;
+        gap: 3.5rem;
+        padding: 1rem;
+      }
+
+      &__property {
+        margin: 0;
+
+        &--0,
+        &--1,
+        &--2 {
+          margin-top: 1rem;
+        }
+      }
+
+      &__image {
+        height: 48rem;
+      }
+
+      &__svg {
+        width: 6.5rem;
+        height: 6.5rem;
+      }
+    }
+  }
+
+  @media only screen and (max-width: 768px) {
+    .properties-section {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 40rem;
+    }
+  }
+
+  @media only screen and (max-width: 500px) {
+    .properties-section {
+      height: 68rem;
     }
   }
 </style>
