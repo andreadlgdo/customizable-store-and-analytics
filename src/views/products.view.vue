64d686b0ce127e8b5e86223db5d9a1a5f184dbfc
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2024-11-26 19:15:47 +0100

    feat: add all products

--- src/views/products.view.vue
+++ src/views/products.view.vue
@@ -2,23 +2,33 @@
   <header-layout colored />
   <div :class="baseClass">
     <base-text tag="h3">{{ category?.toUpperCase() }}</base-text>
+    <base-text v-for="product in products" :key="product.id">
+      {{ product.name }}
+    </base-text>
   </div>
   <Footer />
 </template>
 
 <script lang="ts" setup>
-  import { computed } from 'vue';
+  import { computed, onMounted } from 'vue';
   import { useRoute } from 'vue-router';
 
   import { BaseText, Footer } from '../components';
+  import { useProducts } from '../composables';
 
   import HeaderLayout from './header-layout.view.vue';
 
   const baseClass = 'products';
 
+  const { products, loadProducts } = useProducts();
+
   const route = useRoute();
 
   const category = computed<string>(() => route.params.category as string);
+
+  onMounted(async () => {
+    await loadProducts();
+  });
 </script>
 
 <style lang="scss" scoped>
