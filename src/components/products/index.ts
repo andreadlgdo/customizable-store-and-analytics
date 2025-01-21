3a678a5f33344be07a61ba48613c7fa97279d5be
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-01-21 18:09:19 +0100

    feat: add products table

--- src/components/products/index.ts
+++ src/components/products/index.ts
@@ -1,2 +1,3 @@
 export { default as BaseProductCard } from './base-product-card.component.vue';
 export { default as ProductCarousel } from './product-carousel.component.vue';
+export { default as ProductsTable } from './products-table.component.vue';
