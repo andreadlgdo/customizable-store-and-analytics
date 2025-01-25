35cccc0af5f26c95efbe70f6e301badb9362063d
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-01-25 15:41:55 +0100

    feat: add updateProduct method

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,3 +1,5 @@
+import { Product } from '../interfaces';
+
 const apiUrl = process.env.VUE_APP_API_URL;
 
 export const productService = {
@@ -16,5 +18,26 @@ export const productService = {
       console.log('error', error);
       throw error;
     }
+  },
+  updateProduct: async function (productData: Product) {
+    if (!productData._id) {
+      console.log('The product does not have an id');
+      return;
+    }
+
+    const response = await fetch(`${apiUrl}/api/products/${productData._id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(productData)
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to update product');
+    }
+
+    const responseJson = await response.json();
+    return responseJson.product;
   }
 };
