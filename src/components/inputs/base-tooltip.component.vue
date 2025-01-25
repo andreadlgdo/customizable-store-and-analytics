c2087cb5f69fb584167f511417465d07861fa47a
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-01-25 14:11:40 +0100

    feat: add base tooltip

---
+++ src/components/inputs/base-tooltip.component.vue
@@ -0,0 +1,27 @@
+<template>
+  <base-text :class="baseClass" tag="small" color="main">{{ text }}</base-text>
+</template>
+
+<script lang="ts" setup>
+  import BaseText from '../base-text.component.vue';
+
+  const baseClass = 'base-tooltip';
+
+  defineProps({
+    text: {
+      type: String,
+      required: true
+    }
+  });
+</script>
+
+<style lang="scss" scoped>
+  .base-tooltip {
+    position: absolute;
+    top: -22px;
+    background-color: var(--color-main);
+    padding: 4px 8px;
+    z-index: 999;
+    pointer-events: none;
+  }
+</style>
--- src/components/inputs/index.ts
+++ src/components/inputs/index.ts
@@ -2,6 +2,7 @@ export { default as BaseButton } from './base-button.component.vue';
 export { default as BaseKeywords } from './base-keywords.component.vue';
 export { default as BaseTextInput } from './base-text-input.component.vue';
 export { default as BaseToggle } from './base-toggle.component.vue';
+export { default as BaseTooltip } from './base-tooltip.component.vue';
 export { default as ButtonInput } from './button-input.component.vue';
 export { default as CheckboxInput } from './checkbox-input.component.vue';
 export { default as PasswordInput } from './password-input.component.vue';
