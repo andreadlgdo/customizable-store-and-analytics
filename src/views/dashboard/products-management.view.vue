93e546e22f62336ed796d2b2fc92daa0ad2fbd98
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-01-28 20:47:47 +0100

    feat: add new routes

--- src/views/dashboard/products-management.view.vue
+++ src/views/dashboard/products-management.view.vue
@@ -1,60 +1,85 @@
 <template>
-  <base-text tag="h3">
-    {{
-      formProduct
-        ? itemToEdit
-          ? 'Edit'
-          : t('dashboard.products.title.form.create')
-        : t('dashboard.products.title.list')
-    }}
-  </base-text>
-  <section :class="baseClass">
-    <div v-if="!formProduct" :class="`${baseClass}__wrapper ${baseClass}__wrapper--list`">
-      <base-button
-        @click="addProduct"
-        icon="plus"
-        :text="t('dashboard.products.action.add')"
-        color="primary"
-        :class="`${baseClass}__button`"
-      />
-      <products-table @edit="item => editProduct(item)" />
-    </div>
-    <div v-else :class="`${baseClass}__wrapper ${baseClass}__wrapper--add`">
-      <product-form @action="formProduct = false" :item-to-edit="itemToEdit" />
-    </div>
-  </section>
+  <dashboard :class="baseClass" index="1">
+    <base-text tag="h3">
+      {{
+        formProduct
+          ? itemToEdit
+            ? 'Edit'
+            : t('dashboard.products.title.form.create')
+          : t('dashboard.products.title.list')
+      }}
+    </base-text>
+    <section :class="`${baseClass}__section`">
+      <div v-if="!formProduct" :class="`${baseClass}__wrapper ${baseClass}__wrapper--list`">
+        <base-button
+          @click="addProduct"
+          icon="plus"
+          :text="t('dashboard.products.action.add')"
+          color="primary"
+          :class="`${baseClass}__button`"
+        />
+        <products-table @edit="item => editProduct(item)" />
+      </div>
+      <div v-else :class="`${baseClass}__wrapper ${baseClass}__wrapper--add`">
+        <product-form @action="formProduct = false" :item-to-edit="itemToEdit" />
+      </div>
+    </section>
+  </dashboard>
 </template>
 
 <script lang="ts" setup>
   import { ref } from 'vue';
   import { useI18n } from 'vue-i18n';
+  import { useRouter } from 'vue-router';
 
   import { BaseButton, BaseText, ProductForm, ProductsTable } from '../../components';
   import { Product } from '../../interfaces';
 
+  import Dashboard from '../dashboard.view.vue';
+
   const { t } = useI18n();
+  const router = useRouter();
 
   const baseClass = 'products-management';
 
-  const formProduct = ref(false);
+  const props = defineProps({
+    action: {
+      type: String,
+      default: undefined
+    }
+  });
+
+  const formProduct = ref(!!props.action);
 
   const itemToEdit = ref<Product | undefined>(undefined);
 
   const addProduct = () => {
     formProduct.value = true;
     itemToEdit.value = undefined;
+    router.push({
+      name: 'ProductsDashboard',
+      params: { action: 'add' }
+    });
   };
 
   const editProduct = (item: Product) => {
     itemToEdit.value = item;
     formProduct.value = true;
+    router.push({
+      name: 'ProductsDashboard',
+      params: { action: 'edit' }
+    });
   };
 </script>
 
 <style lang="scss" scoped>
   .products-management {
-    height: 100%;
-    overflow: hidden;
+    position: relative;
+
+    &__section {
+      height: 100%;
+      overflow: hidden;
+    }
 
     &__wrapper {
       display: flex;
