147fb811dd97ca4d73775ef2cedc353c8eb52164
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-02-13 23:02:51 +0100

    feat: add update mode on user form

--- src/views/personal-data.view.vue
+++ src/views/personal-data.view.vue
@@ -4,7 +4,7 @@
       <section :class="`${baseClass}__section`">
         <h1 :class="`${baseClass}__text ${baseClass}__text--title`">Datos personales</h1>
         <div :class="`${baseClass}__wrapper ${baseClass}__wrapper--header`">
-          <ui-image :image="user.imageUrl" />
+          <ui-image :image="user.imageUrl" :uploadMode="updateMode" />
           <div :class="`${baseClass}__wrapper ${baseClass}__wrapper--user`">
             <p :class="`${baseClass}__text ${baseClass}__text--name`">
               {{ user.name + ' ' + user.surname }}
@@ -14,12 +14,20 @@
         </div>
         <div :class="`${baseClass}__wrapper ${baseClass}__wrapper--form`">
           <div :class="`${baseClass}__wrapper ${baseClass}__wrapper--header`">
-            <ui-textbox label="Nombre" :value="user.name" disabled />
-            <ui-textbox label="Apellido" :value="user.surname" disabled />
+            <ui-textbox label="Nombre" :value="user.name" :disabled="!updateMode" />
+            <ui-textbox label="Apellido" :value="user.surname" :disabled="!updateMode" />
           </div>
-          <ui-textbox label="Email" :value="user.email" disabled />
+          <ui-textbox label="Email" :value="user.email" :disabled="!updateMode" />
         </div>
-        <ui-button text="Editar perfil" icon="edit" :class="`${baseClass}__button`" transparent />
+        <div :class="`${baseClass}__button`">
+          <ui-button
+            @click="updateMode = !updateMode"
+            :text="updateMode ? 'Cancelar' : 'Editar perfil'"
+            :icon="updateMode ? 'close' : 'edit'"
+            transparent
+          />
+          <ui-button v-if="updateMode" text="Guardar cambios" icon="edit" />
+        </div>
       </section>
       <section :class="`${baseClass}__section`">
         <h1>Direcciones</h1>
@@ -30,17 +38,23 @@
 </template>
 
 <script lang="ts" setup>
-  import Dashboard from './dashboard.view.vue';
-  import { useCurrentUser, useUserMenu } from '../composables';
+  import { ref } from 'vue';
+
   import UiImage from '../components/shared/ui-image.component.vue';
   import UiButton from '../components/shared/ui-button.component.vue';
   import UiTextbox from '../components/shared/ui-textbox.component.vue';
+
+  import { useCurrentUser, useUserMenu } from '../composables';
+
+  import Dashboard from './dashboard.view.vue';
 
   const { menuElements } = useUserMenu();
 
   const { user } = useCurrentUser();
 
   const baseClass = 'personal-data';
+
+  const updateMode = ref(false);
 </script>
 
 <style lang="scss" scoped>
@@ -93,6 +107,8 @@
     }
 
     &__button {
+      display: flex;
+      gap: 1rem;
       justify-self: flex-end;
     }
   }
