2cacbc5a5db9f9c9bd86bdb6ac5a3c30e2d54c5b
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-02-13 21:09:45 +0100

    refactor: refactor folder name

--- src/views/dashboard/index.ts
+++ src/views/dashboard/index.ts
@@ -1,3 +1,3 @@
 export { default as PersonalDataManagement } from './personal-data-management.view.vue';
-export { default as ProductsManagement } from './products-management.view.vue';
-export { default as UserManagement } from './users-management.view.vue';
+export { default as OldProductsManagement } from './old-products-management.view.vue';
+export { default as OldUserManagement } from './old-users-management.view.vue';
