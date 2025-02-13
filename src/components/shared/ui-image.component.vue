cf1b8383273328f25c325d2ec2152ab8ab5f4c85
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-02-13 21:56:42 +0100

    feat: add ui image component

---
+++ src/components/shared/ui-image.component.vue
@@ -0,0 +1,26 @@
+<template>
+  <img
+    :class="baseClass"
+    :src="image ?? require('../../assets/media/images/empty-user.png')"
+    :alt="alternativeText ?? 'image'"
+  />
+</template>
+
+<script lang="ts" setup>
+  const baseClass = 'ui-image';
+
+  defineProps({
+    image: {
+      type: String,
+      default: undefined
+    },
+    alternativeText: String
+  });
+</script>
+
+<style lang="scss" scoped>
+  .ui-image {
+    height: 8rem;
+    width: 8rem;
+  }
+</style>
