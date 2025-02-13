7be1fb3c1a7414765fcf07dbe4e09cda6e403602
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-02-13 23:02:10 +0100

    feat: add upload image button

--- src/components/shared/ui-image.component.vue
+++ src/components/shared/ui-image.component.vue
@@ -1,12 +1,22 @@
 <template>
-  <img
-    :class="baseClass"
-    :src="image ?? require('../../assets/media/images/empty-user.png')"
-    :alt="alternativeText ?? 'image'"
-  />
+  <span :class="baseClass">
+    <img
+      :class="`${baseClass}__image`"
+      :src="image ?? require('../../assets/media/images/empty-user.png')"
+      :alt="alternativeText ?? 'image'"
+    />
+    <icon-button
+      v-if="uploadMode"
+      icon="close"
+      tooltip-text="Upload new image"
+      :class="`${baseClass}__icon`"
+    />
+  </span>
 </template>
 
 <script lang="ts" setup>
+  import IconButton from './ui-icon-button.component.vue';
+
   const baseClass = 'ui-image';
 
   defineProps({
@@ -14,13 +24,27 @@
       type: String,
       default: undefined
     },
-    alternativeText: String
+    alternativeText: String,
+    uploadMode: Boolean
   });
 </script>
 
 <style lang="scss" scoped>
   .ui-image {
-    height: 8rem;
-    width: 8rem;
+    position: relative;
+
+    &__image {
+      height: 8rem;
+      width: 8rem;
+    }
+
+    &__icon {
+      position: absolute;
+      bottom: 0;
+      right: 0;
+      background-color: var(--color-vibrant-primary);
+      padding: 8px;
+      border-radius: 50%;
+    }
   }
 </style>
