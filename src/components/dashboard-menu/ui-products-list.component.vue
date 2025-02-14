69cdd8900dd6853d076f67003b193484da8d8f97
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-02-14 17:09:39 +0100

    feat: add product list

---
+++ src/components/dashboard-menu/ui-products-list.component.vue
@@ -0,0 +1,62 @@
+<template>
+  <div :class="baseClass">
+    <ui-table
+      @edit="item => $emit('edit', item)"
+      @delete="item => $emit('delete', item)"
+      :columns="columns"
+      :data="products"
+      editable
+      removable
+    >
+      <template v-slot:categories="{ data: categories }">
+        <span v-if="categories">
+          <base-pill
+            v-for="category in categories"
+            :key="category"
+            :text="category"
+            text-size="small"
+            color="primary"
+          />
+        </span>
+      </template>
+    </ui-table>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { computed, PropType } from 'vue';
+  import { useI18n } from 'vue-i18n';
+
+  import { Product } from '../../interfaces';
+
+  import UiTable from '../shared/ui-table.component.vue';
+
+  import BasePill from '../base-pill.component.vue';
+
+  const { t } = useI18n();
+
+  const baseClass = 'ui-products-list';
+
+  defineProps({
+    products: {
+      type: Array as PropType<Product[]>,
+      required: true
+    }
+  });
+  defineEmits(['edit', 'delete']);
+
+  const columns = computed(() => [
+    { id: '_id', label: t('dashboard.products.table.id') },
+    { id: 'name', label: t('dashboard.products.table.name') },
+    { id: 'categories', label: t('dashboard.products.table.categories') },
+    { id: 'price', label: t('dashboard.products.table.price'), textAlign: 'center' },
+    { id: 'quantity', label: t('dashboard.products.table.stock'), textAlign: 'center' }
+  ]);
+</script>
+
+<style lang="scss" scoped>
+  .ui-products-list {
+    height: 72vh;
+    overflow-y: scroll;
+  }
+</style>
--- src/views/products-management.view.vue
+++ src/views/products-management.view.vue
@@ -1,15 +1,46 @@
 <template>
   <dashboard :selected-item="menuElements[1]">
-    <p>Seccion products management</p>
+    <div :class="baseClass">
+      <h1>Gestión de productos</h1>
+      <ui-button text="Add product" icon="plus" :class="`${baseClass}__button`" />
+      <products-list :products="products" :class="`${baseClass}__table`" />
+    </div>
   </dashboard>
 </template>
 
 <script lang="ts" setup>
-  import { useUserMenu } from '../composables';
+  import { onMounted } from 'vue';
+
+  import ProductsList from '../components/dashboard-menu/ui-products-list.component.vue';
+  import UiButton from '../components/shared/ui-button.component.vue';
+
+  import { useProducts, useUserMenu } from '../composables';
 
   import Dashboard from './dashboard.view.vue';
 
   const { menuElements } = useUserMenu();
+  const { loadProducts, products } = useProducts();
+
+  const baseClass = 'products-management';
+
+  onMounted(async () => await loadProducts());
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .products-management {
+    display: flex;
+    flex-direction: column;
+    margin: 2rem;
+    width: 100%;
+
+    &__button {
+      align-self: flex-end;
+      width: fit-content;
+      margin: 1rem 0;
+    }
+
+    &__pagination {
+      align-self: center;
+    }
+  }
+</style>
