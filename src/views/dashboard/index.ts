b88cee052afb9adcc1e0bc42da92cb4e968139cd
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-02-20 19:16:18 +0100

    feat: delete PersonalDataManagement view

--- src/views/dashboard/index.ts
+++ src/views/dashboard/index.ts
@@ -1,3 +1,2 @@
-export { default as PersonalDataManagement } from './personal-data-management.view.vue';
 export { default as OldProductsManagement } from './old-products-management.view.vue';
 export { default as OldUserManagement } from './old-users-management.view.vue';
