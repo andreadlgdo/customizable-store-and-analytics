39c0d94e68e6483673785326a92cf0df5904aa8e
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-02-20 19:15:12 +0100

    refactor: refactor product service

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -1,3 +1,4 @@
+import { fetchData } from '../services/httpClient';
 import { Product } from '../interfaces';
 
 const apiUrl = process.env.VUE_APP_API_URL;
@@ -10,61 +11,29 @@ export const productService = {
       categories.forEach(category => url.searchParams.append('categories', category));
     }
 
-    const response = await fetch(url);
-    if (!response.ok) {
-      throw new Error('Network response was not ok');
-    }
-
-    return await response.json();
+    return await fetchData(url.toString(), { method: 'GET' });
   },
   createProduct: async function (productData: Product) {
-    const response = await fetch(`${apiUrl}/api/products`, {
+    return fetchData(`${apiUrl}/api/products`, {
       method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
       body: JSON.stringify(productData)
     });
-
-    if (!response.ok) {
-      throw new Error('Failed to create a product');
-    }
-
-    return await response.json();
   },
   updateProduct: async function (productData: Product) {
     if (!productData._id) {
-      console.log('The product does not have an id');
-      return;
+      throw new Error('Product ID is required');
     }
-
-    const response = await fetch(`${apiUrl}/api/products/${productData._id}`, {
+    const response = await fetchData(`${apiUrl}/api/products/${productData._id}`, {
       method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
       body: JSON.stringify(productData)
     });
-
-    if (!response.ok) {
-      throw new Error('Failed to update product');
-    }
-
-    const responseJson = await response.json();
-    return responseJson.product;
+    return response.product;
   },
   deleteProduct: async function (_id: string) {
     if (!_id) {
-      console.log('El producto no tiene un ID');
-      return;
+      throw new Error('Product ID is required');
     }
 
-    const response = await fetch(`${apiUrl}/api/products/${_id}`, {
-      method: 'DELETE'
-    });
-
-    if (!response.ok) {
-      throw new Error('Error al eliminar el producto');
-    }
+    await fetchData(`${apiUrl}/api/products/${_id}`, { method: 'DELETE' });
   }
 };
