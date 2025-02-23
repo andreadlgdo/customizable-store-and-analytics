f0698ce262bdbc670b6da14532cfc018c1d08a80
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-02-23 14:11:33 +0100

    fix: fix home scroll

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,28 @@
 <template>
-  <div class="app">
+  <div :class="['app', routeClass]">
     <router-view />
   </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+  import { computed } from 'vue';
+  import { useRoute } from 'vue-router';
+
+  const route = useRoute();
+
+  const routeClass = computed(() => {
+    switch (route.name) {
+      case 'PersonalData':
+        return 'dashboard';
+      case 'ProductsManagement':
+        return 'dashboard';
+      case 'UsersManagement':
+        return 'dashboard';
+      default:
+        return '';
+    }
+  });
+</script>
 
 <style lang="scss">
   @import 'assets/sass/styles';
@@ -15,5 +33,9 @@
     height: 100%;
     background: var(--bg-main);
     color: var(--color-main);
+  }
+
+  .app.dashboard * {
+    overflow: hidden;
   }
 </style>
