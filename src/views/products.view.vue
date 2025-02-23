693ccded246cc394b1da5c4170aeb53727e82665
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-02-23 15:55:54 +0100

    feat: add filter category to products view

--- src/views/products.view.vue
+++ src/views/products.view.vue
@@ -1,42 +1,64 @@
 <template>
   <div :class="baseClass">
-    <section :class="`${baseClass}__header`"></section>
-    <section :class="`${baseClass}__content`">
-      <ui-product-card v-for="product in products" :key="product._id" :product="product" />
+    <ui-header />
+    <section :class="`${baseClass}__wrapper ${baseClass}__wrapper--content`">
+      <h1 :class="`${baseClass}__text`">
+        {{ category ? category.toUpperCase() : 'ALL CATEGORIES' }}
+      </h1>
+      <section :class="`${baseClass}__wrapper ${baseClass}__wrapper--product`">
+        <ui-product-card v-for="product in products" :key="product._id" :product="product" />
+      </section>
     </section>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { onMounted } from 'vue';
+  import { computed, watch } from 'vue';
+  import { useRoute } from 'vue-router';
 
   import { useProducts } from '../composables';
 
   import UiProductCard from '../components/shared/products/ui-product-cart.component.vue';
+  import UiHeader from '../components/shared/ui-header.component.vue';
 
   const baseClass = 'products';
 
   const { products, loadProducts } = useProducts();
+  const route = useRoute();
 
-  onMounted(async () => await loadProducts());
+  const category = computed<string>(() => (route.params.category as string)?.toLowerCase());
+
+  watch(
+    category,
+    async newCategory => {
+      await loadProducts(newCategory ? [newCategory] : []);
+    },
+    { immediate: true }
+  );
 </script>
 
 <style lang="scss" scoped>
   .products {
     overflow-x: hidden;
 
-    &__header {
-      background: var(--color-soft-primary);
-      height: 5rem;
-      width: 100%;
+    &__wrapper {
+      display: flex;
+      gap: 2rem;
+
+      &--content {
+        flex-direction: column;
+        margin: 4rem;
+      }
+
+      &--product {
+        flex-wrap: wrap;
+        justify-content: center;
+      }
     }
 
-    &__content {
-      display: flex;
-      justify-content: center;
-      flex-wrap: wrap;
-      gap: 2rem;
-      margin: 4rem;
+    &__text {
+      font-size: 22px;
+      font-weight: bold;
     }
   }
 </style>
