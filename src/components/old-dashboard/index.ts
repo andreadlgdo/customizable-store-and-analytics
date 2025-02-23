d33b9c1815f48bb5be573aa41d777a2650cab1d9
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-02-23 14:00:15 +0100

    refactor: remove old dashboard files

--- src/components/old-dashboard/index.ts
+++ src/components/old-dashboard/index.ts
@@ -1,4 +1,2 @@
-export * from './users';
-
 export { default as BaseWrapper } from './base-wrapper.component.vue';
 export { default as EmptyProductView } from './empty-product-view.component.vue';
--- src/components/old-dashboard/users/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as UserForm } from './user-form.component.vue';
-export { default as UsersTable } from './users-table.component.vue';
