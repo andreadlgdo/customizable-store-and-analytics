b83b1e9a84e32876bcfd31146ba2bfabfb242517
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-03-11 20:54:33 +0100

    feat: add ui label legend component

---
+++ src/components/shared/order/ui-label-legend.component.vue
@@ -0,0 +1,45 @@
+<template>
+  <div :class="baseClass">
+    <p
+      :class="[`${baseClass}__label`, { [`${baseClass}__label--selected`]: option.selected }]"
+      v-for="(option, index) in options"
+      :key="index"
+    >
+      {{ index + 1 + '. ' + option.label }}
+    </p>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { PropType } from 'vue';
+
+  const baseClass = 'ui-label-legend';
+
+  interface Legend {
+    label: string;
+    selected: boolean;
+  }
+
+  defineProps({
+    options: {
+      type: Array as PropType<Legend[]>,
+      required: true
+    }
+  });
+</script>
+
+<style lang="scss" scoped>
+  .ui-label-legend {
+    display: flex;
+    justify-content: space-between;
+    margin-bottom: 2rem;
+
+    &__label {
+      font-size: 18px;
+
+      &--selected {
+        font-weight: bold;
+      }
+    }
+  }
+</style>
