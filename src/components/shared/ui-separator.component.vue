08c7863da3611c238a8a0fc6e5f7d725e1fcda2d
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-03-19 17:33:29 +0100

    feat: add ui separator component

---
+++ src/components/shared/ui-separator.component.vue
@@ -0,0 +1,33 @@
+<template>
+  <div :class="baseClass">
+    <span :class="`${baseClass}__text`">
+      {{ label }}
+    </span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  const baseClass = 'ui-separator';
+
+  defineProps({
+    label: {
+      type: String,
+      required: true
+    }
+  });
+</script>
+
+<style lang="scss" scoped>
+  .ui-separator {
+    border-bottom: 1px solid var(--color-medium);
+    text-align: center;
+    align-items: center;
+
+    &__text {
+      background-color: white;
+      padding: 0 10px;
+      position: relative;
+      top: 8px;
+    }
+  }
+</style>
