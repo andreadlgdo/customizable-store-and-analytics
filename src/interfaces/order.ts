1032fd75d790e7620ba907e2770245457c540ab9
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-03-19 18:53:00 +0100

    feat: improve order interface

--- src/interfaces/order.ts
+++ src/interfaces/order.ts
@@ -1,6 +1,8 @@
 export interface Order {
   _id?: string;
   userId: string;
+  user?: UserOrder;
+  address?: AddressOrder;
   status: string;
   products: ProductOrder[];
   promotionCode?: string;
@@ -11,4 +13,19 @@ export interface ProductOrder {
   units: string;
   size: string;
   productId: string;
+}
+
+export interface UserOrder {
+  name: string;
+  surname: string;
+  email: string;
+}
+
+export interface AddressOrder {
+  street: string;
+  number: string;
+  letter: string;
+  zipCode: string;
+  city: string;
+  country: string;
 }
