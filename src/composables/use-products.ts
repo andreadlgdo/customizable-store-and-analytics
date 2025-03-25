98fccd9924884a1bc736fc4b0481ffafb4b0524b
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-03-25 17:16:30 +0100

    feat: add findProduct method on use products composable

--- src/composables/use-products.ts
+++ src/composables/use-products.ts
@@ -1,16 +1,20 @@
 import { ref } from 'vue';
 
 import { productService } from '../services';
+import { Product } from '../interfaces';
 
 export function useProducts() {
-  const products = ref([]);
+  const products = ref<Product[]>([]);
 
   const loadProducts = async (categories?: string[]) => {
     products.value = await productService.getProducts(categories);
   };
 
+  const findProduct = (productId: string) => products.value.find(p => p._id === productId);
+
   return {
     products,
-    loadProducts
+    loadProducts,
+    findProduct
   };
 }
