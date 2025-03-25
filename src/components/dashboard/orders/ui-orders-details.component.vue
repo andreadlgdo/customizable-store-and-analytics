64088f890f2fdbf48c89aedf9fec04f8369e0ea4
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-03-25 17:32:52 +0100

    feat: add order details component

---
+++ src/components/dashboard/orders/ui-orders-details.component.vue
@@ -0,0 +1,167 @@
+<template>
+  <div :class="baseClass">
+    <div v-if="item" :class="`${baseClass}__wrapper ${baseClass}__wrapper--column`">
+      <div :class="`${baseClass}__wrapper ${baseClass}__wrapper--row`">
+        <ui-textbox :value="item._id" :label="t('dashboard.orders.form.id')" disabled />
+        <ui-textbox :value="item.status" :label="t('dashboard.orders.form.status')" disabled />
+        <ui-textbox :value="item.total + ' €'" :label="t('dashboard.orders.form.total')" disabled />
+      </div>
+      <div :class="`${baseClass}__wrapper ${baseClass}__wrapper--row`">
+        <ui-textbox :value="item.userId" :label="t('dashboard.orders.form.user.idUser')" disabled />
+        <ui-textbox
+          :value="item.user?.name + ' ' + item.user?.surname"
+          :label="t('dashboard.orders.form.user.name')"
+          disabled
+        />
+        <ui-textbox
+          :value="item.user?.email"
+          :label="t('dashboard.orders.form.user.email')"
+          disabled
+        />
+      </div>
+      <ui-textbox :value="address" :label="t('dashboard.orders.form.address')" disabled />
+      <p :class="`${baseClass}__text`">{{ t('dashboard.orders.form.products.title') }}</p>
+      <div :class="`${baseClass}__wrapper ${baseClass}__wrapper--column-products`">
+        <div
+          v-for="product in item.products"
+          :key="product.productId"
+          :class="`${baseClass}__wrapper ${baseClass}__wrapper--product`"
+        >
+          <ui-image
+            :image="findProduct(product.productId)?.imageUrl ?? ''"
+            size="mini"
+            :class="`${baseClass}__image`"
+            type="semi-round"
+          />
+          <ui-textbox
+            :value="product.productId"
+            :label="t('dashboard.orders.form.products.id')"
+            disabled
+          />
+          <ui-textbox
+            :value="findProduct(product.productId)?.name"
+            :label="t('dashboard.orders.form.products.name')"
+            disabled
+          />
+          <ui-textbox
+            :value="
+              findProduct(product.productId)?.priceWithDiscount ??
+              findProduct(product.productId)?.price
+            "
+            :label="t('dashboard.orders.form.products.price')"
+            disabled
+          />
+          <ui-textbox
+            :value="product.units"
+            :label="t('dashboard.orders.form.products.units')"
+            disabled
+            :class="`${baseClass}__textbox`"
+          />
+          <ui-textbox
+            :value="product.size"
+            :label="t('dashboard.orders.form.products.size')"
+            disabled
+            :class="`${baseClass}__textbox`"
+          />
+        </div>
+      </div>
+    </div>
+    <UiButton
+      @click="$emit('cancel')"
+      text="Close"
+      icon="close"
+      :class="`${baseClass}__wrapper ${baseClass}__wrapper--button`"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import { computed, onMounted, PropType } from 'vue';
+  import { useI18n } from 'vue-i18n';
+
+  import { useProducts } from '../../../composables';
+  import { Order } from '../../../interfaces';
+
+  import UiButton from '../../shared/ui-button.component.vue';
+  import UiTextbox from '../../shared/ui-textbox.component.vue';
+  import UiImage from '../../shared/ui-image.component.vue';
+
+  const baseClass = 'ui-order-details';
+
+  const props = defineProps({
+    item: {
+      type: Object as PropType<Order>,
+      required: true
+    }
+  });
+
+  defineEmits(['cancel']);
+
+  const { loadProducts, findProduct } = useProducts();
+  const { t } = useI18n();
+
+  const address = computed(() =>
+    props.item.address
+      ? props.item.address.street +
+        ', ' +
+        props.item.address.number +
+        'º' +
+        props.item.address.letter +
+        ', ' +
+        props.item.address.zipCode +
+        ', ' +
+        props.item.address.city +
+        ', ' +
+        props.item.address.country
+      : ''
+  );
+
+  onMounted(async () => await loadProducts());
+</script>
+
+<style lang="scss" scoped>
+  .ui-order-details {
+    position: relative;
+    margin: 2rem;
+    height: 100%;
+
+    &__wrapper {
+      display: flex;
+      gap: 1rem;
+      overflow: visible !important;
+
+      &--column,
+      &--column-products {
+        flex-direction: column;
+      }
+
+      &--column-products {
+        overflow-y: scroll;
+        height: 22rem;
+      }
+
+      &--product {
+        align-items: center;
+      }
+
+      &--button {
+        position: fixed;
+        bottom: 2rem;
+        right: 2rem;
+      }
+    }
+
+    &__text {
+      font-weight: bold;
+      font-size: 22px;
+    }
+
+    &__image {
+      width: 22rem;
+    }
+
+    &__textbox {
+      width: 16rem;
+    }
+  }
+</style>
--- src/views/dashboard/orders-management.view.vue
+++ src/views/dashboard/orders-management.view.vue
@@ -1,15 +1,71 @@
 <template>
   <dashboard :selected-item="menuElements[2]">
-    <p>Seccion orders management</p>
+    <div :class="baseClass">
+      <h1 v-if="!itemId" :class="`${baseClass}__title`">{{ t('dashboard.orders.title') }}</h1>
+      <ui-orders-list v-if="!itemId" @seeDetails="seeOrderDetails" :orders="orders" />
+      <ui-orders-details
+        v-else
+        @cancel="closeDetails"
+        :item="orders.find((o: Order) => o._id === itemId)"
+      />
+    </div>
   </dashboard>
 </template>
 
 <script lang="ts" setup>
+  import { onMounted, ref } from 'vue';
+  import { useI18n } from 'vue-i18n';
+  import { useRouter } from 'vue-router';
+
+  import UiOrdersDetails from '../../components/dashboard/orders/ui-orders-details.component.vue';
+  import UiOrdersList from '../../components/dashboard/orders/ui-orders-list.component.vue';
+
   import { useUserMenu } from '../../composables';
+  import { Order } from '../../interfaces';
+  import { orderService } from '../../services';
 
   import Dashboard from './dashboard.view.vue';
 
+  const baseClass = 'orders-management';
+
+  defineProps({
+    itemId: {
+      type: String,
+      default: undefined
+    }
+  });
+
   const { menuElements } = useUserMenu();
+  const { t } = useI18n();
+  const router = useRouter();
+
+  const orders = ref<Order[]>([]);
+
+  const seeOrderDetails = (item: Order) => {
+    router.push({
+      name: 'OrdersManagement',
+      params: { itemId: item._id }
+    });
+  };
+
+  const closeDetails = () => {
+    router.push({
+      name: 'OrdersManagement'
+    });
+  };
+
+  onMounted(async () => (orders.value = await orderService.getOrders()));
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .orders-management {
+    display: flex;
+    flex-direction: column;
+    margin: 2rem;
+    width: 100%;
+
+    &__title {
+      margin: 1rem 0;
+    }
+  }
+</style>
