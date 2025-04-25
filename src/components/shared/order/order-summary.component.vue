4d8921a98f8b61dd6e25ee9d58e241a4ef03de77
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-04-25 20:00:09 +0200

    fix: fix scroll on order summary

--- src/components/shared/order/order-summary.component.vue
+++ src/components/shared/order/order-summary.component.vue
@@ -1,42 +1,44 @@
 <template>
-  <div :class="baseClass">
-    <div :class="`${baseClass}__wrapper ${baseClass}__wrapper--column`">
-      <h2 :class="`${baseClass}__title`">{{ t('order.options.personalData') }}</h2>
-      <div v-if="userRegister" :class="`${baseClass}__wrapper ${baseClass}__wrapper--row`">
-        <UiTextbox :value="user.name" :label="t('asides.register.form.name')" disabled />
-        <UiTextbox :value="user.surname" :label="t('asides.register.form.surname')" disabled />
-      </div>
-      <UiTextbox :value="user.email" label="Email" disabled />
-      <h2 :class="`${baseClass}__title`">{{ t('order.address.title') }}</h2>
-      <UiAddress :addresses="[address]" :editable="false" disabled />
-      <h2 :class="`${baseClass}__title`">{{ t('order.payment.title') }}</h2>
+  <div :class="`${baseClass}-container`">
+    <div :class="baseClass">
       <div :class="`${baseClass}__wrapper ${baseClass}__wrapper--column`">
-        <UiTextbox :value="payment.owner" :label="t('order.payment.owner')" disabled />
-        <UiTextbox
-          :value="payment.cardNumber"
-          :label="t('order.payment.cardNumber.label')"
-          disabled
-        />
-        <div :class="`${baseClass}__wrapper ${baseClass}__wrapper--row`">
-          <UiTextbox :value="payment.month" :label="t('order.payment.month.label')" disabled />
-          <UiTextbox :value="payment.year" :label="t('order.payment.year.label')" disabled />
-          <UiTextbox :value="payment.cvv" :label="t('order.payment.cvv.label')" disabled />
+        <h2 :class="`${baseClass}__title`">{{ t('order.options.personalData') }}</h2>
+        <div v-if="userRegister" :class="`${baseClass}__wrapper ${baseClass}__wrapper--row`">
+          <UiTextbox :value="user.name" :label="t('asides.register.form.name')" disabled />
+          <UiTextbox :value="user.surname" :label="t('asides.register.form.surname')" disabled />
+        </div>
+        <UiTextbox :value="user.email" label="Email" disabled />
+        <h2 :class="`${baseClass}__title`">{{ t('order.address.title') }}</h2>
+        <UiAddress :addresses="[address]" :editable="false" disabled />
+        <h2 :class="`${baseClass}__title`">{{ t('order.payment.title') }}</h2>
+        <div :class="`${baseClass}__wrapper ${baseClass}__wrapper--column`">
+          <UiTextbox :value="payment.owner" :label="t('order.payment.owner')" disabled />
+          <UiTextbox
+            :value="payment.cardNumber"
+            :label="t('order.payment.cardNumber.label')"
+            disabled
+          />
+          <div :class="`${baseClass}__wrapper ${baseClass}__wrapper--row`">
+            <UiTextbox :value="payment.month" :label="t('order.payment.month.label')" disabled />
+            <UiTextbox :value="payment.year" :label="t('order.payment.year.label')" disabled />
+            <UiTextbox :value="payment.cvv" :label="t('order.payment.cvv.label')" disabled />
+          </div>
         </div>
       </div>
     </div>
-  </div>
-  <div :class="`${baseClass}__footer`">
-    <UiButton
-      @click="$emit('back')"
-      :text="t('order.action.back')"
-      :class="`${baseClass}__button`"
-      transparent
-    />
-    <UiButton
-      @click="$emit('finish')"
-      :text="t('order.action.finish')"
-      :class="`${baseClass}__button`"
-    />
+    <div :class="`${baseClass}__footer`">
+      <UiButton
+        @click="$emit('back')"
+        :text="t('order.action.back')"
+        :class="`${baseClass}__button`"
+        transparent
+      />
+      <UiButton
+        @click="$emit('finish')"
+        :text="t('order.action.finish')"
+        :class="`${baseClass}__button`"
+      />
+    </div>
   </div>
 </template>
 
@@ -75,10 +77,11 @@
   const { user: userRegister } = useUsers();
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" scoped>  
   .order-summary {
-    max-height: 32rem;
-    overflow-y: scroll;
+    flex: 1;
+    overflow-y: auto;
+    max-height: 80%;
 
     &__title {
       font-weight: bold;
@@ -95,12 +98,13 @@
 
     &__footer {
       position: absolute;
-      bottom: 24px;
+      bottom: 88px;
       right: 0;
       display: flex;
       justify-content: space-between;
       padding: 0 2rem;
       width: 100%;
+      background-color: var(--bg-main);
     }
   }
 </style>
