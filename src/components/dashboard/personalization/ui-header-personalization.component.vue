6d21256116d8b5bda32216e1c7c82b6a51930c55
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-04-27 13:28:31 +0200

    feat: add ui header personalization component

---
+++ src/components/dashboard/personalization/ui-header-personalization.component.vue
@@ -0,0 +1,35 @@
+<template>
+     <div @click="router.push({ name: 'Personalization' })" :class="baseClass">
+        <ui-icon-button icon="go-to" :class="`${baseClass}__icon`"/>
+        <p :class="`${baseClass}__text`">Volver al menu</p>
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { useRouter } from 'vue-router';
+import UiIconButton from '../../shared/ui-icon-button.component.vue';
+
+const baseClass = 'ui-header-personalization';
+
+const router = useRouter();
+</script>
+
+<style lang="scss" scoped>
+.ui-header-personalization {
+    display: flex;
+    align-items: center;
+    position: absolute;
+    left: 21rem;
+    cursor: pointer;
+
+    &__icon {
+        transform: rotate(180deg);
+    }
+
+    &__text {
+        &:hover {
+         font-weight: 600;
+        }
+    }
+}
+</style>
