f47d2f3d65a20e3588d912f3b9f74a25c92a0264
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-04-27 13:29:44 +0200

    feat: add ui contact us preview

---
+++ src/components/dashboard/personalization/contact-us/ui-contact-us-preview.component.vue
@@ -0,0 +1,29 @@
+<template>
+    <div :class="baseClass">
+        <h1 :class="`${baseClass}__title`">
+            {{ t('contactUs.title') }}
+        </h1>
+        <UiContactForm />
+        <UiContactMethods />
+    </div>
+</template>
+
+<script lang="ts" setup>
+import { useI18n } from 'vue-i18n';
+
+import UiContactForm from '../../../contact-us/ui-contact-form.component.vue';
+import UiContactMethods from '../../../contact-us/ui-contact-methods.component.vue';
+
+const baseClass = 'ui-contact-us-preview';
+
+const { t } = useI18n();
+</script>
+
+<style lang="scss" scoped>
+.ui-contact-us-preview {
+    &__title {
+        margin-bottom: 1.5rem;
+        text-align: center;
+    }
+}
+</style>
