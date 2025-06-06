4385e0c3394b12f31ea61fcae55720a51b3874ef
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-06-06 11:38:39 +0200

    refactor: remove Footer component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,4 +3,3 @@ export * from './inputs';
 
 export { default as BasePill } from './base-pill.component.vue';
 export { default as BaseText } from './base-text.component.vue';
-export { default as Footer } from './footer.component.vue';
