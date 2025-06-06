9d844ddfa231224a60cdc46061781ce4d03a26f1
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-06-06 11:46:22 +0200

    refactor: remove BaseKeywords and BaseTextInput components

--- src/components/inputs/index.ts
+++ src/components/inputs/index.ts
@@ -1,5 +1,3 @@
-export { default as BaseKeywords } from './base-keywords.component.vue';
-export { default as BaseTextInput } from './base-text-input.component.vue';
 export { default as BaseToggle } from './base-toggle.component.vue';
 export { default as BaseTooltip } from './base-tooltip.component.vue';
 export { default as ButtonInput } from './button-input.component.vue';
