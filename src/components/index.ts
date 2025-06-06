826534cc239746807e82d6a390ce8584b8a874be
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-06-06 11:29:59 +0200

    refactor: remove ProductCarousel component

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,9 +1,6 @@
 export * from './header';
 export * from './icons';
 export * from './inputs';
-export * from './menu';
-export * from './products';
-export * from './toggles';
 
 export { default as BasePill } from './base-pill.component.vue';
 export { default as BaseText } from './base-text.component.vue';
