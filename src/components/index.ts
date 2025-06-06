1eac6302a1c1735eb54bd5431d046d2606a1c887
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-06-06 11:27:12 +0200

    refactor: remove ListItems component

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,5 +8,4 @@ export * from './toggles';
 export { default as BasePill } from './base-pill.component.vue';
 export { default as BaseText } from './base-text.component.vue';
 export { default as Footer } from './footer.component.vue';
-export { default as ListItems } from './list-items.component.vue';
 export { default as PropertiesSection } from './properties-section.component.vue';
