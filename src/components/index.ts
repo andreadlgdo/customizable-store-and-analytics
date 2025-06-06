20cde30d56e76ce5866d58ae36b1002e292952ac
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-06-06 11:27:41 +0200

    refactor: remove PropertiesSection component

--- src/components/index.ts
+++ src/components/index.ts
@@ -8,4 +8,3 @@ export * from './toggles';
 export { default as BasePill } from './base-pill.component.vue';
 export { default as BaseText } from './base-text.component.vue';
 export { default as Footer } from './footer.component.vue';
-export { default as PropertiesSection } from './properties-section.component.vue';
