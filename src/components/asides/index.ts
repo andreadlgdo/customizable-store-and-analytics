d1e8b951f89de39939e28946b1fece59b131860e
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-06-06 11:21:13 +0200

    refactor: remove MenuAside component

--- src/components/asides/index.ts
+++ src/components/asides/index.ts
@@ -1,4 +1,3 @@
 export { default as LogInAside } from './log-in-aside.component.vue';
-export { default as MenuAside } from './menu-aside.component.vue';
 export { default as ShopAside } from './shop-aside.component.vue';
 export { default as SignUpAside } from './sign-up-aside.component.vue';
