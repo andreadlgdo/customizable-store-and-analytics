e6f9194387e57e22ecceb0a3db6ed00038886adb
Andrea Delgado <114981520+andreadlgdo@users.noreply.github.com>
2025-07-25 09:02:00 +0200

    feat: optimize for production

--- vue.config.js
+++ vue.config.js
@@ -12,5 +12,56 @@ module.exports = defineConfig({
         `
       }
     }
+  },
+  configureWebpack: {
+    optimization: {
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all',
+          },
+          icons: {
+            test: /[\\/]assets[\\/]media[\\/]icons[\\/]/,
+            name: 'icons',
+            chunks: 'all',
+            minChunks: 1,
+            enforce: true,
+          }
+        }
+      }
+    }
+  },
+  // Configuración para SVG
+  chainWebpack: config => {
+    config.module
+      .rule('svg')
+      .test(/\.svg$/)
+      .use('svgo-loader')
+      .loader('svgo-loader')
+      .options({
+        plugins: [
+          { removeViewBox: false },
+          { removeEmptyAttrs: true },
+          { removeEmptyText: true },
+          { removeEmptyContainers: true },
+          { removeHiddenElems: true },
+          { removeUselessDefs: true },
+          { removeXMLNS: false },
+          { removeEditorsNSData: true },
+          { removeRasterImages: false },
+          { removeUselessStrokeAndFill: true },
+          { removeUnusedNS: true },
+          { removeDesc: true },
+          { removeTitle: true },
+          { removeComments: true },
+          { removeMetadata: true },
+          { removeDoctype: true },
+          { removeXMLProcInst: true },
+          { removeStyleElement: true },
+          { removeScriptElement: true }
+        ]
+      });
   }
 });
